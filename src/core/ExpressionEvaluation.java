62d721029e8fd35697de3766541964d8addcaef9
Eduardo F. Assis <eduardoffassis@gmail.com>
2019-10-09 22:28:00 -0300

    WIP expression evaluation

--- src/core/ExpressionEvaluation.java
+++ src/core/ExpressionEvaluation.java
@@ -1,31 +1,54 @@
 package core;
 
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 import java.util.Vector;
+
+
 
 public class ExpressionEvaluation {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String s = sc.next();
-		Queue<String> q = new LinkedList<>();
-		
-		// tem q atualizar a string dp de pegar o parenteses
-		if (s.contains("(") && s.contains(")")) {
-			int aux1 = s.indexOf("(");
-			int aux2 = s.indexOf(")");
-			
-			String subs = s.substring(aux1 + 1, aux2);
-			q.add(subs);
+		Vector<String> vetor = new Vector<String>();
+				
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == ' ') {
+				
+			}
+			else if (s.charAt(i) == '(') {
+			}
+			else if (s.charAt(i) == ')') {
+			}
+			else if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+				String num = "" + s.charAt(i); 
+				for (int j = i+1; j < s.length(); j++) {					
+					if (s.charAt(j) < '0' || s.charAt(j) > '9') {		// n é num
+						i = j - 1;
+						break;
+					}
+					else {
+						num = num + s.charAt(j);
+					}
+				}
+				vetor.add(num);
+			}
+			else if (s.charAt(i) == '+' || s.charAt(i) == '-') {
+				vetor.add("" + s.charAt(i));
+			}
+			else {
+				System.out.println("Expression error!");
+				break;
+			}
 		}
 		
-		sc.close();
-		
-		for(String str : q) {
-			System.out.println(str);
+		if (vetor.size() != 0) {
+			// System.out.println((int) ' ');
+			for (String str : vetor) {
+				System.out.println(str);
+			}
 		}
+	sc.close();
 	}
 
 }
